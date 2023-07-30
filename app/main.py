f7e6ac6b9cb4c9de30c0187bfcaf2d639c4e26cb
astelmach01 <astelmach20@gmail.com>
2023-07-30 00:26:48 -0700

    added pip and poetry caching

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from app.settings import settings
 
 app = FastAPI(
-    title=settings.project_name, openapi_url=f"{settings.API_V1_STR}/openapi.json"
+    title=settings.project_name, openapi_url=f"/{settings.API_V1_STR}/openapi.json"
 )
 
 # include all the routers here
--- main.py
+++ main.py
@@ -7,6 +7,5 @@ from app.settings import settings
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8080))
-    reload = settings.environment != "prod"
 
-    uvicorn.run(app, host="0.0.0.0", port=port, reload=reload)
+    uvicorn.run(app, host="0.0.0.0", port=port)
