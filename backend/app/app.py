a9aa0d4747ffacd81723d111eebf2588be79777c
astelmach01 <astelmach20@gmail.com>
2023-08-07 22:56:39 -0700

    test cache

--- backend/app/app.py
+++ backend/app/app.py
@@ -54,7 +54,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "ello World"}
+    return {"message": "Hello World"}
 
 
 def verify_password(plain_password, hashed_password):
