1e96ca708cf71ddcd779d599a660d4b2445bf9ab
astelmach01 <astelmach20@gmail.com>
2023-08-16 20:42:17 -0700

    small fix

--- backend/app/web/api/router.py
+++ backend/app/web/api/router.py
@@ -1,6 +1,8 @@
-from app.web.api import docs, monitoring
 from fastapi.routing import APIRouter
 
+from .docs import router as docs_router
+from .monitoring import router as monitoring_router
+
 api_router = APIRouter()
-api_router.include_router(monitoring.router)
-api_router.include_router(docs.router)
+api_router.include_router(monitoring_router)
+api_router.include_router(docs_router)
