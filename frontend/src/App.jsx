be14435d75f902bc90deb282c362f4a16ef0a1dc
astelmach01 <astelmach20@gmail.com>
2023-08-17 21:34:56 -0700

    added crappy frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,25 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import './App.css';
+import FloatingCircle from './FloatingCircle';
 
 function App() {
   const [message, setMessage] = useState('');
-
+  const sendMessage = async (text) => {
+    try {
+      await axios.post('/chat', { message: text });
+      console.log('Message sent:', text);
+    } catch (error) {
+      console.error('Error sending message:', error);
+    }
+  };
   const handleKeyDown = async (e) => {
     if (e.keyCode === 13) {
       e.preventDefault();
       if (message.trim() !== '') {
-        // eslint-disable-next-line no-use-before-define
         await sendMessage(message);
         setMessage('');
       }
-    }
-  };
-
-  const sendMessage = async (text) => {
-    try {
-      await axios.post('/chat', { message: text });
-    } catch (error) { /* empty */
     }
   };
 
@@ -28,6 +28,7 @@ function App() {
       <header className="App-header">
         <h1>Cosmo Chat</h1>
       </header>
+      <FloatingCircle />
       <main className="App-main">
         <div className="message-box">
           <textarea
